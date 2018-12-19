6f7f59753e3f0f7dc44a2b4aaa92942b2fb8ebb6
Dan Kreitz <dan.kreitz@outlook.com>
2018-12-19 15:51:22 -0500

    minor changes to TestDropDowns

--- Module6/src/classroom/TestDropDowns.java
+++ Module6/src/classroom/TestDropDowns.java
@@ -10,14 +10,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class TestDropDowns {
-	
+
 	WebDriver driver;
 	Select dropDown;
 	List<WebElement> categoryList;
-	
+
 	public void invokeBrowser() {
 
-		System.setProperty("webdriver.chrome.driver", "C:\\Java\\Tools\\Selenium\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Code\\Java\\Tools\\Selenium\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
@@ -27,27 +27,26 @@ public class TestDropDowns {
 		testDropDown();
 
 	}
-	
+
 	public void testDropDown() {
-		
+
 		WebElement dropDownElement = driver.findElement(By.id("searchDropdownBox"));
 		dropDown = new Select(dropDownElement);
 		System.out.println("Is multiple allowed: " + dropDown.isMultiple());
 		categoryList = dropDown.getOptions();
-		
-		for (WebElement category: categoryList) {
-			System.out.println(category);
+
+		for (WebElement category : categoryList) {
+			System.out.println(category.getText());
 		}
-		
-//		dropDown.selectByVisibleText("Books");
-//		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Roblox");
-//		driver.findElement(By.xpath("//*[@id=\"nav-search\"]//input[@value='Go']")).submit();
-		
+
+		dropDown.selectByValue("search-alias=stripbooks");
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Roblox");
+		driver.findElement(By.xpath("//*[@id=\"nav-search\"]//input[@value='Go']")).submit();
+
 	}
-	
 
 	public static void main(String[] args) {
-		
+
 		TestDropDowns obj = new TestDropDowns();
 		obj.invokeBrowser();
 
