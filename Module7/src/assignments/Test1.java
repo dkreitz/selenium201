27064154b778c4ea3784e72c48efc4720f68e8e0
Dan Kreitz <dan.kreitz@outlook.com>
2018-12-28 11:59:31 -0500

    completed assignments for Module7

--- Module7/src/assignments/Test1.java
+++ Module7/src/assignments/Test1.java
@@ -1,10 +1,21 @@
 package assignments;
 
+import static org.testng.Assert.assertEquals;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Test1 {
@@ -15,14 +26,86 @@ public class Test1 {
 	 * page irrespective of the fact that the test case is passing or failing (use
 	 * TestNG assertion to verify this). As the work on one page gets completed take
 	 * a screenshot.
+	 * 
+	 * Search on Google how to include a timestamp along with the name of
+	 * screenshot. If you don’t include a timestamp, all the screenshots will be
+	 * overwritten. Figure out how to overcome this.
 	 */
 
 	WebDriver driver;
+	String imageFolder = "screenshots\\";
+	File tempFile, permanentFile;
+	TakesScreenshot screenShot;
 
 	@Test
+	public void openMercuryTours() {
+
+		driver.get("http://newtours.demoaut.com");
+
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "Welcome: Mercury Tours";
+
+		assertEquals(actualTitle, expectedTitle);
+
+	}
+
+	@Test(dependsOnMethods = { "openMercuryTours" })
+	public void loginToMercuryTours() {
+
+		driver.findElement(By.name("userName")).sendKeys("dkreitz");
+		driver.findElement(By.name("password")).sendKeys("dkreitz");
+		driver.findElement(By.name("login")).click();
+
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "Find a Flight: Mercury Tours:";
+
+		assertEquals(actualTitle, expectedTitle);
+
+	}
+
+	@Test(dependsOnMethods = { "loginToMercuryTours" })
+	public void findFlights() {
+
+		// use the default values and continue
+		driver.findElement(By.name("findFlights")).click();
+
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "Select a Flight: Mercury Tours";
+
+		assertEquals(actualTitle, expectedTitle);
+
+	}
+
+	@Test(dependsOnMethods = { "findFlights" })
+	public void selectFlights() {
+
+		// use the default values and continue
+		driver.findElement(By.name("reserveFlights")).click();
+
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "Book a Flight: Mercury Tours";
+
+		assertEquals(actualTitle, expectedTitle);
+
+	}
+
+	@Test(dependsOnMethods = { "selectFlights" })
+	public void bookFlights() {
+
+		// use the default values and continue
+		driver.findElement(By.name("buyFlights")).click();
+
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "Flight Confirmation: Mercury Tours";
+
+		assertEquals(actualTitle, expectedTitle);
+
+	}
+
+	@BeforeTest
 	public void openBrowser() {
 
-		System.setProperty("webdriver.chrome.driver", "C:\\Java\\Tools\\Selenium\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Code\\Java\\Tools\\Selenium\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
@@ -31,17 +114,35 @@ public class Test1 {
 
 	}
 
-	@Test(dependsOnMethods = { "openBrowser" })
-	public void openMercuryTours() {
-		driver.get("http://www.facebook.com");
+	@AfterMethod
+	public void takeScreenshot() {
+		System.out.print("Say cheese... ");
+
+		String timestamp = new SimpleDateFormat("yyyy_MM_dd__hh_mm_ss_SSS").format(new Date());
+		String imageFilename = imageFolder + "newtours_" + timestamp + ".png";
+
+		try {
+
+			permanentFile = new File(imageFilename);
+			if (permanentFile.exists()) {
+				throw new Exception("Image already exists:" + imageFilename);
+			}
+			screenShot = (TakesScreenshot) driver;
+			tempFile = screenShot.getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(tempFile, permanentFile);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+		
+		System.out.println("Screenshot taken: " + imageFilename);
 	}
 
-	@Test(dependsOnMethods = { "openMercuryTours" })
-	public void loginToMercuryTours() {
-
-		driver.findElement(By.cssSelector("#email")).sendKeys("dkreitz@gmail.com");
-		driver.findElement(By.cssSelector("#pass")).sendKeys("password");
-		driver.findElement(By.cssSelector("#loginbutton")).click();
-
+	@AfterTest
+	public void afterAllTests() {
+		driver.quit();
 	}
+
 }
--- Module7/src/assignments/Test2.java
+++ Module7/src/assignments/Test2.java
@@ -1,16 +1,48 @@
 package assignments;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Test2 {
 
 	/*
-	 * Search on Google how to include a timestamp along with the name of
-	 * screenshot. If you don’t include a time-stamp, all the screenshots will be
-	 * overwritten. Figure out how to overcome this?
+	 * Create a program to go to https://www.dropbox.com/. Print the title of the
+	 * page 5 times. Each time a title is printed, browser should get closed. To get
+	 * the title again, the browser should open again.
 	 */
+	
+	WebDriver driver;
 
-	@Test
-	public void f() {
+	@Test(invocationCount = 5)
+	public void openDropbox() {
+		
+		driver.get("https://www.dropbox.com");
+		String myTitle = driver.getTitle();
+		System.out.println("My title is: " + myTitle);
+		
 	}
+
+	@BeforeMethod
+	public void openBrowser() {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Code\\Java\\Tools\\Selenium\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(45, TimeUnit.SECONDS);
+
+	}
+
+	@AfterMethod
+	public void closeBrowser() {
+		driver.quit();
+	}
+
+	
 }
