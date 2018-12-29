5b672bd1ed370918df4adfcf56b62c01e82242b7
Dan Kreitz <dan.kreitz@outlook.com>
2018-12-29 17:08:41 -0500

    updated GmailPage with code from slide #13 from Presentation

--- Module8/src/pages/GmailPage.java
+++ Module8/src/pages/GmailPage.java
@@ -1,22 +1,52 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class GmailPage {
-	
+
 	WebDriver driver;
-	By gmailUser = By.name("uid");
-	By gmailPassword = By.name("password");
-	By gmailTitle = By.className("barry");
-	By gmailLoginBtn = By.name("btnlogin");
-	
-	public void gmailLogin(WebDriver driver) {
-		this.driver = driver;		
+
+	@FindBy(name = "uid")
+	WebElement usergmail;
+
+	@FindBy(name = "password")
+	WebElement passwordGmail;
+
+	@FindBy(className = "barry")
+	WebElement titleText;
+
+	@FindBy(name = "btnLogin")
+	WebElement login;
+
+	public GmailPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
-	
+
 	public void setUserName(String strUserName) {
-		driver.findElement(gmailUser).sendKeys(strUserName);
+		usergmail.sendKeys(strUserName);
+	}
+
+	public void setPassword(String strPassword) {
+		passwordGmail.sendKeys(strPassword);
+	}
+
+	public void clickLogin() {
+		login.click();
+	}
+
+	public String getLoginTitle() {
+		return titleText.getText();
+
+	}
+
+	public void loginGmail(String strUserName, String strPassword) {
+		this.setUserName(strUserName);
+		this.setPassword(strPassword);
+		this.clickLogin();
 	}
 
 }
